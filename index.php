2a2b478e11aa9ee3026b6447f35f5edb1bfb70e2
Pablo Sepulveda <pablosep@msn.com>
2022-01-22 18:50:52 -0800

    Transfer files to Git folder

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php include "includes/header.php";?>
+ 
+<!-- *** HEADER for PHP cut ends here -->
+
+<!-- MAKE SURE YOU GET YOUR (3) IMAGES SAVED INTO YOUR IMAGES FOLDER -->
+ <img src="images/desktop.jpg" class="desktop" alt="" />
+ <img src="images/phone.jpg" class="phone" alt="" />
+ <img src="images/tablet.jpg" class="tablet" alt="" />
+
+</section>
+<!-- END LEFT COL -->
+
+<!-- START RIGHT COL -->
+<aside>
+ <h3>About me</h3>
+ <p>Hello, my name is Pablo Sepulveda. I was born and 
+  raised in Chile. I immigrated to the US when I was 18 years old. 
+  I studied music and flute in college.
+  Now I work independently as a Spanish interpreter, translator and voice-over 
+  artist. I enjoy learning 
+  about computers and technology. I also enjoy exercising and learning ASL language.</p>
+<p>In 2020, most of my work disappeared because of the pandemic.
+  While in lockdown, with entire days to fill at home, I decided to upgrade my skills, 
+  and I studied computer
+   technologies at North Seattle college, earning a Web Application
+    Technologies Certificate. I am enthusiastic about learning more
+ front-end web design and development.</p>
+</aside>
+<!-- END RIGHT COL -->
+<?php include "includes/footer.php";?>
---
+++ template.php
@@ -0,0 +1,8 @@
+<?php include "includes/header.php";?>
+ 
+<!-- This is a template to start new pages -->
+<h2 class="subheader">Template</h2>
+<p>Use this as a model fore creating additional pages</p>
+
+
+<?php include "includes/footer.php";?>
